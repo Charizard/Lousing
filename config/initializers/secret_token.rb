092dfd037c58b0fdc1a7861a6f943a8f329c19f5
Yuvaraja Balamurugan <yuva@germ.io>
2014-12-08 13:13:54 +0530

    Initial commit for Lousing.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lousing::Application.config.secret_key_base = '5c551cc3b5ed60267c51204dc6161847ac42fb22ac77663b7d87e38f54317ed29d6fea182f34ae3946cf83e765be467c988490535a43a8ae8d25549ed9d9dbfc'
