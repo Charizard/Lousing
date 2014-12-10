ee219b6f7d9238cb4005444a9bac915822d46b7f
Yuvaraja Balamurugan <yuva@germ.io>
2014-12-10 19:12:34 +0530

    Added the tabs for displaying the shortlisted properties

--- app/assets/javascripts/routes/listings_new_route.js
+++ app/assets/javascripts/routes/listings_new_route.js
@@ -1,5 +1,5 @@
 Lousing.ListingsNewRoute = Ember.Route.extend({
   beforeModel: function(transition){
-    return Lousing.isLoggedIn(this, transition) && Lousing.isLandlord(this, transition);
+    // return Lousing.isLoggedIn(this, transition) && Lousing.isLandlord(this, transition);
   }
 });
