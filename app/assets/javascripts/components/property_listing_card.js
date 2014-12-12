13cfcfb487c86530122c700fb6536896566946ce
Yuvaraja Balamurugan <yuva@germ.io>
2014-12-12 12:55:04 +0530

    Fixed the property_listing_card as the properties_listing object passed to the component was a controller instance rather than a model instance

--- app/assets/javascripts/components/property_listing_card.js
+++ app/assets/javascripts/components/property_listing_card.js
@@ -4,9 +4,8 @@ Lousing.PropertyListingCardComponent = Ember.Component.extend({
   action: "toggleShortlist",
   isShortlisted: function() {
     var self = this;
-    // Why is the following commented line not working, but the line below it is working perfectly fine?
-    // return this.get('current_user.short_listed_properties').contains(this.get('property_listing'));
-    return this.get('current_user.short_listed_properties').map(function(p){ return p.get('id');}).contains(this.get('property_listing.id'));
+
+    return this.get('current_user.short_listed_properties').contains(this.get('property_listing'));
   }.property('property_listing.shortlisted_users.length'),
   actions: {
     toggleShortlist: function(){
