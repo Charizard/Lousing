0700a139426428eabd26d9f2a410c39d0355f087
Yuvaraja Balamurugan <yuva@germ.io>
2014-12-15 19:05:30 +0530

    Shortlisted modal displayed

--- app/assets/javascripts/components/property_detail_table.js
+++ app/assets/javascripts/components/property_detail_table.js
@@ -1,5 +1,8 @@
 Lousing.PropertyDetailTableComponent = Ember.Component.extend({
   shortlisted_count: Ember.computed.alias('property_listing.shortlisted_users.length'),
+  pluralize_count: function(){
+    return (this.get('shortlisted_count') == 1) ? "person" : "people";
+  }.property('shortlisted_count'),
   actions: {
     contact_agent: function(){
       var self = this;
