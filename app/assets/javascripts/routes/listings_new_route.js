14bccc86f907263c3f818990c6ef63e4472229f6
Yuvaraja Balamurugan <yuva@germ.io>
2014-12-15 15:15:42 +0530

    PropertyListing creation now happens using createRecord

--- app/assets/javascripts/routes/listings_new_route.js
+++ app/assets/javascripts/routes/listings_new_route.js
@@ -1,5 +1,28 @@
 Lousing.ListingsNewRoute = Ember.Route.extend({
-  beforeModel: function(transition){
-    // return Lousing.isLoggedIn(this, transition) && Lousing.isLandlord(this, transition);
+  model: function() {
+    return this.get('store').createRecord('property_listing', {});
+  },
+  exit: function(){
+    var model = this.get('currentModel');
+    if(model.get('isNew') && !model.get('isSaving')) {
+      return model.rollback();
+    }
+  },
+  actions: {
+    create: function(){
+      var newListing = this.get('currentModel');
+      newListing.set('poster', this.currentUser);
+
+      var self = this;
+      newListing.save().then(
+        function(){
+          self.transitionTo('index');
+          Lousing.Alert('success', "Successfully created the Property Listing");
+        },
+        function(){
+          Lousing.Alert('danger', "Cannot create Listing");
+        }
+      );
+    }
   }
 });
